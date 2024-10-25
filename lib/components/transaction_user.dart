61aaefec47545b4d1ad4e03f9b8a4a989d29cf74
Joao Gabriel Chiarello <joao.chiarello@unochapeco.edu.br>
2024-10-25 14:05:14 -0300

    Dividido components da main para novos files

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({super.key});
+
+  @override
+  State<TransactionUser> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<TransactionUser> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gestao/components/transaction_form.dart';
+import 'package:gestao/components/transaction_list.dart';
 import 'package:gestao/models/transaction.dart';
-import 'package:intl/intl.dart';
 
 main() => runApp(ExpenseApp());
 
@@ -16,9 +17,6 @@ class ExpenseApp extends StatelessWidget {
 }
 
 class Homepage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
   final _transactions = [
     Transaction(
       id: 't1',
@@ -50,89 +48,8 @@ class Homepage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Column(
-              children: _transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat('d MM y').format(tr.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
-            Card(
-              elevation: 5,
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      controller: titleController,
-                      decoration: InputDecoration(
-                        labelText: 'Título',
-                      ),
-                    ),
-                    TextField(
-                      controller: valueController,
-                      decoration: InputDecoration(
-                        labelText: 'Valor (R\$)',
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            print(titleController.text);
-                            print(valueController.text);
-                          },
-                          child: Text(
-                            'Nova Transação',
-                            style: TextStyle(color: Colors.purple),
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
+            // TransactionList(_transactions),
+            // TransactionForm(),
           ],
         ));
   }
